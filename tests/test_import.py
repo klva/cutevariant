e6c0cbd208fb44cf17ca8559eaf663088f0c6585
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-12-16 17:15:56 +0100

    use fixtures

--- tests/test_import.py
+++ tests/test_import.py
@@ -7,97 +7,90 @@ from cutevariant.core.importer import import_file
 from cutevariant.core.model import create_session, Variant,Field,Selection
 from cutevariant.core.query import QueryBuilder
 
-'''
-connect to database 
-'''
-path = "/tmp/cutevariant.db"
-if os.path.exists(path):
-    os.remove(path)
 
-engine = sqlalchemy.create_engine(f"sqlite:///{path}", echo=False)
+@pytest.fixture
+def engine():
+    '''
+    connect to database
+    '''
+    path = "/tmp/cutevariant.db"
+    print('PATH:', path, os.path.exists(path))
+    if os.path.exists(path):
+        os.remove(path)
+    print('PATH:', path, os.path.exists(path))
 
-
-def test_import_csv():
-    # import file 
+    engine = sqlalchemy.create_engine(f"sqlite:///{path}", echo=False)
     import_file("exemples/test.csv", engine)
+    return engine
 
-    # test data 
+
+def test_csv_data(engine):
     session = create_session(engine)
-    assert session.query(Variant).count() == 5
+    assert session.query(Variant).count() == 6
     assert session.query(Field).count() == 4
+    session.close()
 
-
-def test_query():
-    builder = QueryBuilder(engine)
-    builder.fields = ["chr","pos","ref"]
-    builder.condition = "variants.id > 3"
-
-    print(builder)
-
-    for i in builder.query():
-        print(i)
-
-    print("list")
-    for i in builder.to_list():
-        print(i)
-
-
-
-def test_query_selection():
-    builder = QueryBuilder(engine)
-    builder.fields = ["chr","pos"]
-
-    A = builder.query()
-
-
-    builder.condition = "variants.id > 3"
-    builder.create_selection("sacha")
-
+def test_csv_data_2(engine):
     session = create_session(engine)
+    assert session.query(Variant).count() == 6
+    assert session.query(Field).count() == 4
+    session.close()
 
 
-    builder.selection_name = "sacha"
-    builder.condition = "variants.chr == 'chr8'"
+# def test_query(engine):
+    # builder = QueryBuilder(engine)
+    # builder.fields = ["chr","pos","ref"]
+    # builder.condition = "variants.id > 3"
 
+    # print(builder)
 
-    print("selection A")
-    for i in A:
-        print(i)
+    # for i in builder.query():
+        # print(i)
 
-    B  = builder.query()
-
-    print("selection B")
-    for i in B:
-        print(i)
-
-
-    print("selection C")
-
-    C = A.except_(B)
-
-    for i in C:
-        print(i)
-
-    print("selection")
-    query = session.query(Variant).join(Selection, Variant.selections).filter(Selection.name == "sacha")
-    for i in query.filter(sqlalchemy.text("variants.id > 0")):
-        print(i)
-    
+    # print("list")
+    # for i in builder.to_list():
+        # print(i)
 
 
 
+# def test_query_selection(engine):
+    # builder = QueryBuilder(engine)
+    # builder.fields = ["chr","pos"]
+
+    # A = builder.query()
 
 
+    # builder.condition = "variants.id > 3"
+    # builder.create_selection("sacha")
+
+    # session = create_session(engine)
 
 
+    # builder.selection_name = "sacha"
+    # builder.condition = "variants.chr == 'chr8'"
 
 
+    # print("selection A")
+    # for i in A:
+        # print(i)
+
+    # B  = builder.query()
+
+    # print("selection B")
+    # for i in B:
+        # print(i)
 
 
+    # print("selection C")
 
+    # C = A.except_(B)
 
+    # for i in C:
+        # print(i)
 
+    # print("selection")
+    # query = session.query(Variant).join(Selection, Variant.selections).filter(Selection.name == "sacha")
+    # for i in query.filter(sqlalchemy.text("variants.id > 0")):
+        # print(i)
 
-
-
-
+    # session.close()
